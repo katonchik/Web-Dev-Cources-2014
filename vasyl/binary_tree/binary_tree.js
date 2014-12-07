1a6c2a422230c497e20553573607954f61d7b423
Vasyl <vatres@ukr.net>
2014-12-07 08:57:50 +0200

    fixed binary tree and add helpful function
    add new methods and functions

--- vasyl/binary_tree/binary_tree.js
+++ vasyl/binary_tree/binary_tree.js
@@ -1,84 +1,150 @@
-function Tree(tree_height) {
-    var first = true;
-    //var tree;
+Binary_Tree = function (set) {
+    var this_ = this;
+    var left = 0;
+    var right = 1;
+    var count = 0;
+    var arr_set = set;
+    var t_root =[];
 
-    function Branch(branch_height) {
-        var this_ = this;
-        this.root = [];
-        var height = branch_height;
-        var number = Math.round(Math.random() * 100);
-        this_.number = number;
-        this_.height = height;
+    
+    
+    this.create=function(){
+        t_root=inseretion();
+        this.t_root=t_root;
+    };
 
-        var width = 0;
+    this.search = function (number) {
+        var that=t_root;
+        if(is_num_in_array(arr_set,number)){
+            while (that.number != number) {
+                // console.log(that.number, number, 2);
+                if (that.number > number) {
+                    // console.log(that, number, "l");
 
-        this_.create_random_tree = function () {
+                    that = that.baranch_root[left];
+                } else {
+                    // console.log(that, number, "r");
 
-            if (first) {
-                width = 1;
-                first = false;
-            }
-            if (height < tree_height - 1)width = Math.round(Math.random() * 2);
-            console.log(number, height, first, width);
-            ++height;
-            if (height < tree_height) {
-
-                for (var i = 0; i < width; i++) {
-                    this_.root[i] = new Branch(height)
+                    that = that.baranch_root[right];
                 }
             }
-        };
-        if (height < tree_height)
-            this_.create_random_tree();
-    }
-
-
-    var branches = new Branch(0);
-    // branches.create_random_tree();
-    //
-    this.add_branches = function (height, width, number) {
-        var count = 0;
-
-        function add(that) {
-            if (height - 1 > count) {
-                count++;
-                //console.log(that, "i'm here");
-                return add(that.root[width - 1]);
-            }
-            return that;
+            //console.log(that.number);
+            return that;}
+        else{
+            console.log("    oops")
+            return null;
         }
-
-        var a = add(branches);
-        console.log(a);
-        var length = a.root.length;
-        a.root[length] = new Branch();
-        //  a.root[length].number=number;
-        a.root[length].height = height;
-        // console.log(a.pop());
     };
-    //
-    this.delete_branches = function (height, width) {
-        var count = 0;
+    
+    
+    find_min_max=function(par,that){
+        if(par==1)
+            var parametr=left;
+        else(parametr=right);
 
-        function add(that) {
-            if (height > count + 1) {
-                count++;
-                //console.log(that, "i'm here");
-                return add(that.root[0]);
-            }
-            return that;
+        while(that!=null){
+            if (that.baranch_root[left] == null)
+                break;
+            that=  that.baranch_root[parametr];}
+        return that;
+    }
+    function find_succesor(that){
+        var succesor =find_min_max(1,that.baranch_root[right]);
+        return succesor;
+
+    }
+    
+    
+    this.remove = function (number) {
+        var that=  this.search(number);
+        var succesor=null;
+        if(that.child==0){
+            console.log("0");
+            that=null;
+        }else if(that.child==1)
+        {
+            console.log("1");
+            that=that.baranch_root[left]||that.baranch_root[right];
+        }else{
+            console.log("2");
+            succesor=find_succesor(that);
+            console.log(succesor);
+            this.remove(succesor.number);
+            that=succesor;
         }
 
-        var a = add(branches);
-        console.log(a);
-        a.root[width - 1] = null;
-    }
-    this.out = function () {
-        console.log(branches);
-    }
-}
-var t = new Tree(3);
 
-//t.add_branches(2, 2, 45);
-//t.delete_branches(1,1);
+    };
+    
+    
+
+    this.add = function (array) {
+        arr_set = make_set(arr_set.concat(array));
+        inseretion();
+    };
+    
+    
+    function inseretion() {
+        var root;
+        var that;
+        while (count < arr_set.length) {
+            var number = arr_set[count];
+            if (count == 0) {
+                root = new Branch(number,0);
+                //console.log(root, number, 1);
+                count++;
+                continue;
+            }
+
+            that = root;
+            while (that != null) {
+                // console.log(that.number, number, 2);
+                if (that.number > number) {
+                    //console.log(that, number, "l");
+                    if (that.baranch_root[left] == null) {
+                        that.child++;
+                        that = that.baranch_root[left] = new Branch(number,0);
+                        break;
+                    }
+                    that = that.baranch_root[left];
+                } else {
+                    // console.log(that, number, "r");
+                    if (that.baranch_root[right] == null) {
+                        that.child++;
+                        that = that.baranch_root[right] = new Branch(number,0);
+                        break;
+                    }
+                    that = that.baranch_root[right];
+                }
+            }
+            // console.log(root, 3);
+            count++;
+        }
+        return root;
+    }
+    
+
+    function Branch(number,child) {
+        var this_ = this;
+        this_.child=child;
+        this_.baranch_root = [null, null];
+        this_.number = number;
+    }
+
+    
+    this.out = function () {
+        console.log(arr_set.sort(function (a, b) {
+            return a - b
+        }), arr_set.length);
+        console.log(t_root);
+    };
+
+};
+array=genrator_random_set(10);
+console.log(array);
+var t= new Binary_Tree(array);
+t.create();
 t.out();
+
+//console.log(t.t_root);
+//t.add().
