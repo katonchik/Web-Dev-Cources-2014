fefa188885646dd56023ed966adef3b078cd4693
Mykhailo Ivankiv <mykhailo.ivankiv@teamvoy.com>
2014-12-07 15:49:16 +0200

    Added binary tree. (finnaly )))

--- orest/binary tree/binary_tree.js
+++ orest/binary tree/binary_tree.js
@@ -1,35 +1,84 @@
 // Binary Tree
-
-var someArray = [5, 2, 8, 6, 1, 7, 3, 9, 4, 10]
+var someArray = [5, 2, 8, 6, 1, 7, 3, 9, 4, 10];
 
 // Creating an Object using a function
-function BinaryTreeCreate(someArray, value) {
-    this.root = null;
-    this.value = value;
+function BinaryTree() {
+    this.tree = {}
 }
 
-BinaryTreeCreate.prototype.setLeft = function(left) {
-     this.left = left;
-}
-
-BinaryTreeCreate.prototype.setRight = function(right) {
-     this.right = right;
-}
-
-BinaryTreeCreate.prototype.add = function(value) {
-    var node = {
-        value: value;
-        left: null;
-        right: null;
+BinaryTree.prototype.add = function (value) {
+    function domWalk (value, subtree) {
+        if (subtree.value > newNode.value ) {
+            if (subtree.left === undefined) {
+                subtree.left = newNode
+            } else {
+                domWalk (value, subtree.left)
+            }
+        } else {
+            if (subtree.right === undefined) {
+                subtree.right = newNode
+            } else {
+                domWalk (value, subtree.right)
+            }
+        }
     };
-    var current;
 
-    //check if this is the first node of a tree
-    if (this.root === null) {
-        this.root = node;
+    var newNode = {
+        value : value,
+        left : undefined,
+        right : undefined
+    };
+
+    if (this.tree.value) {
+        domWalk (value, this.tree);
+    } else {
+        this.tree = newNode;
     }
-    else {
-        current = node;
-    }
-}
+
+
+};
+
+
+BinaryTree.prototype.render = function () {
+    function renderNode (node) {
+        var leftNode = "";
+        var rightNode = "";
+
+        if (node.left) {
+            leftNode = renderNode(node.left);
+        }
+
+        if (node.right) {
+            rightNode = renderNode(node.right);
+        }
+
+        return '' +
+          '<div class="dom-node">' +
+            '<div class="node-value">' + node.value + '</div>' +
+            '<div class="left-node">' + leftNode + '</div>' +
+            '<div class="right-node">' + rightNode + '</div>' +
+        '</div>'
+    };
+
+
+    var rootEl = document.createElement("div");
+
+    rootEl.innerHTML = renderNode(this.tree);
+    return rootEl;
+};
+
+
+BinaryTree.prototype.asdfas  = 1;
+
+var binaryTree = new BinaryTree();
+
+someArray.forEach(function (el) {
+    binaryTree.add(el);
+});
+
+console.log(binaryTree.tree);
+
+var binaryView = binaryTree.render();
+document.body.appendChild(binaryView);
+
 
