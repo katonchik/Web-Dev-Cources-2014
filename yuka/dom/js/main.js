fcc111e9e9e0d956ae3180f1365f2e4550e892ed
yuka <mezarecordija@gmail.com>
2014-12-26 22:44:29 +0200

    added simple visualisation of insertion sort.

--- yuka/dom/js/main.js
+++ yuka/dom/js/main.js
@@ -1,27 +1,51 @@
 window.onload = function () {
+    /**
+     * @param data
+     * @returns {HTMLElement}
+     */
     function render (data) {
         var div = document.createElement("div");
-        div.innerHTML = data;
+        div.className = 'array-element';
+        div.style.height = data + "px";
         return div;
+    }
+
+    /**
+     * @param data {Array}
+     * @return Array {Array}
+     */
+    function sortInsertion(data) {
+        for (var i = 0; i < data.length; i++) {
+            var smallest = data[i];
+
+
+            var j = i - 1;
+            while (j >= 0 && data[j] > smallest) {
+                data[j + 1] = data[j];
+                j--;
+            }
+            data[j + 1] = smallest;
+        }
+        return data;
     }
 
     var newArray = generateRandomArray(25);
 
-    var section = document.getElementById("sortVisualization");
+    var section = document.getElementsByClassName("sort-visualization")[0];
 
     newArray.map(render).forEach(function(arrayData) {
         section.appendChild(arrayData);
     });
 
-    document.getElementById("startSorting").addEventListener("click", function() {
+    document.getElementsByClassName("start-sorting")[0].addEventListener("click", function() {
         sortInsertion(newArray);
-        var anotherArray = newArray.map(render);
+        var sortedArray = newArray.map(render);
 
         section.innerHTML = "";
 
-        anotherArray.forEach(function(node) {
+        sortedArray.forEach(function(node) {
             section.appendChild(node);
-        })
+        });
     });
 };
 
