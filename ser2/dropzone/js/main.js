0527f76cbd2c791911aaa1148985021865c6f73b
sergejgasjuk <sergejgasjuk@gmail.com>
2015-01-24 22:38:04 +0200

    removed dropzone old

--- ser2/dropzone/js/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    var dropbox = document.getElementById("dropbox");
-
-    // init event handlers
-    dropbox.addEventListener("dragenter", dragEnter, false);
-    dropbox.addEventListener("dragexit", dragExit, false);
-    dropbox.addEventListener("dragover", dragOver, false);
-    dropbox.addEventListener("drop", drop, false);
-}
