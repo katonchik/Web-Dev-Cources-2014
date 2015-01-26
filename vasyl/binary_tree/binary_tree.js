66e73bf5869a423875bddc33fe11119f16007493
Vasyl <vatres@ukr.net>
2015-01-26 16:39:27 +0200

    update binary tree functional

--- vasyl/binary_tree/binary_tree.js
+++ vasyl/binary_tree/binary_tree.js
@@ -1,180 +1,172 @@
-Binary_Tree = function (set) {
-    var this_ = this;
-    var left = 0;
-    var right = 1;
-    var count = 0;
-    var arr_set = set; // Array of values of tree
-    var t_root =[];
-
+BinaryTree = function () {
+    this.tree = {};
 
     /**
-     * create binary tree
+     * searchValue (int)- value that you want to find
+     * function return array first is subtree which contains found value, second is parent that contains subtree
+     * , and the third pointer from parent to subtree
+     * @param searchValue
+     * @returns {Array}
      */
-    this.create=function(){
-        t_root=inseretion();
-        this.t_root=t_root;
-    };
-    /**
-     *
-     * @param number - value that you want to find
-     * @returns {Branch}
-     */
-    this.search = function (number) {
-        var that=t_root;
-        if(is_num_in_array(arr_set,number)){
-            while (that.number != number) {
-                // console.log(that.number, number, 2);
-                if (that.number > number) {
-                    // console.log(that, number, "l");
+    this.search = function (searchValue) {
+        var searchResult=[];
 
-                    that = that.baranch_root[left];
+        function searchValue(value, subtree) {
+            searchResult[0] = subtree;
+            if (subtree.value == value) {
+
+            } else {
+                if (subtree.value > value) {
+                    searchResult[1] = subtree;
+                    searchResult[2] = "left";
+                    searchValue(value, subtree.left);
                 } else {
-                    // console.log(that, number, "r");
-
-                    that = that.baranch_root[right];
+                    searchResult[1] = subtree;
+                    searchResult[2] = "right";
+                    searchValue(value, subtree.right);
                 }
             }
-            //console.log(that.number);
-            return that;}
-        else{
-            console.log("    oops")
-            return null;
-        }
-    };
-
-    /**
-     * function find min or max value
-     * @param par if value of param =1 that you find min element of tree else max
-     * @param that(Branch)
-     * @returns {Branch}
-     */
-    find_min_max=function(par,that){
-        if(par==1)
-            var parametr=left;
-        else(parametr=right);
-
-        while(that!=null){
-            if (that.baranch_root[left] == null)
-                break;
-            that=  that.baranch_root[parametr];}
-        return that;
-    }
-    /**
-     *
-     * @param that(Branch)
-     * @returns {Branch}
-     */
-    function find_succesor(that){
-        var succesor =find_min_max(1,that.baranch_root[right]);
-        return succesor;
-
-    }
-    
-    
-    this.remove = function (number) {
-        var that=  this.search(number);
-        var succesor=null;
-        if(that.child==0){
-            console.log("0");
-            that=null;
-        }else if(that.child==1)
-        {
-            console.log("1");
-            that=that.baranch_root[left]||that.baranch_root[right];
-        }else{
-            console.log("2");
-            succesor=find_succesor(that);
-            console.log(succesor);
-            this.remove(succesor.number);
-            that=succesor;
         }
 
-
-    };
-
-
-    /**
-     * add new branches
-     * @param array(Array) -Array of values which you want to add
-     */
-    this.add = function (array) {
-        arr_set = make_set(arr_set.concat(array));
-        inseretion();
-    };
-
-    /**
-     * inseret branches in tree
-     * @returns {Branch}
-     */
-    function inseretion() {
-        var root;
-        var that;
-        while (count < arr_set.length) {
-            var number = arr_set[count];
-            if (count == 0) {
-                root = new Branch(number,0);
-                //console.log(root, number, 1);
-                count++;
-                continue;
-            }
-
-            that = root;
-            while (that != null) {
-                // console.log(that.number, number, 2);
-                if (that.number > number) {
-                    //console.log(that, number, "l");
-                    if (that.baranch_root[left] == null) {
-                        that.child++;
-                        that = that.baranch_root[left] = new Branch(number,0);
-                        break;
-                    }
-                    that = that.baranch_root[left];
-                } else {
-                    // console.log(that, number, "r");
-                    if (that.baranch_root[right] == null) {
-                        that.child++;
-                        that = that.baranch_root[right] = new Branch(number,0);
-                        break;
-                    }
-                    that = that.baranch_root[right];
-                }
-            }
-            // console.log(root, 3);
-            count++;
-        }
-        return root;
-    }
-
-    /**
-     *
-     * @param number -value of this branch
-     * @param child - number of child
-     * @constructor
-     */
-    function Branch(number,child) {
-        var this_ = this;
-        this_.child=child;
-        this_.baranch_root = [null, null];
-        this_.number = number;
-    }
-
-    /**
-     * show sorted  Array of values of tree
-     * and show tree root
-     */
-    this.out = function () {
-        console.log(arr_set.sort(function (a, b) {
-            return a - b
-        }), arr_set.length);
-        console.log(t_root);
+        searchValue(searchValue, this.tree);
+        return searchResult;
     };
 
 };
-array=genrator_random_set(10);
-console.log(array);
-var t= new Binary_Tree(array);
-t.create();
-t.out();
+/**
+ * add new tree leaf with defined value(Int)
+ * @param value
+ */
+BinaryTree.prototype.add = function (value) {
 
-//console.log(t.t_root);
-//t.add().
+    function treeWalk(value, subtree) {
+        if (subtree.value > value) {
+            if (subtree.left == null) {
+                subtree.child++;
+                subtree.left = newNode;
+
+            } else {
+                treeWalk(value, subtree.left)
+            }
+
+        } else {
+            if (subtree.right == null) {
+                subtree.child++;
+                subtree.right = newNode;
+
+            }
+            else {
+                treeWalk(value, subtree.right)
+            }
+        }
+    }
+
+    var newNode = {
+        left: null,
+        right: null,
+        child: 0,
+        value: value
+    };
+    if (this.tree.value) {
+        treeWalk(value, this.tree);
+    } else {
+        this.tree = newNode;
+    }
+};
+/**
+ * create binary tree
+ * @param numberOfElements
+ */
+BinaryTree.prototype.create = function (numberOfElements) {
+    var array = generatorRandomSet(numberOfElements);
+    array.forEach(function (el) {
+        binaryTree.add(el)
+    });
+};
+/**par -subtree instance in which we going to search min max values
+ * switcher -Boolean , using to switch search between min(true) and max(false) values
+ * @param subtree
+ * @param switcher
+ * @returns {Object}
+ */
+BinaryTree.prototype.searchMinMaxValueOfSubtree = function (subtree, switcher) {
+    var result;
+    var parameter;
+    var subtree = this.subtree;
+    if (switcher) {
+        parameter = "left";
+    }
+    else {
+        parameter = "right";
+    }
+    function search(subtree) {
+        result = subtree;
+        if (subtree[parameter] == null) {
+
+
+        } else {
+            search(subtree[parameter]);
+        }
+    }
+    search(subtree);
+
+    return result;
+};
+/**
+ * remove element(leaf) of binary tree with defined value
+ * @param value
+ */
+BinaryTree.prototype.remove = function (value) {
+    var search=this.search(value);
+    var subtree = search[0];
+    var parentSubtree=search[1];
+    var pointer=search[2];
+    var successor;
+//binary tree removing algorithms
+//if subtree without children then it will be removed
+//if subtree with one child then it will be replaced onto child
+//if subtree with children then it will be replaced onto the last right child(child with the biggest value)
+    if (subtree.child == 0) {
+        parentSubtree[pointer] = null;
+        console.log("0",subtree);
+    } else if (subtree.child == 1) {
+        parentSubtree[pointer] = subtree.left || subtree.right;
+    } else {
+        successor = this.searchMinMaxValueOfSubtree(subtree, false);
+        console.log(successor);
+        this.remove(successor.value);
+        parentSubtree[pointer].value = successor.value;
+    }
+};
+
+
+BinaryTree.prototype.render = function () {
+    function renderNode(node) {
+        var leftNode = "";
+        var rightNode = "";
+        if (node.left) {
+            leftNode = renderNode(node.left);
+        }
+        if (node.right) {
+            rightNode = renderNode(node.right);
+        }
+        return '' +
+            '<div class="dom-node">' +
+            '<div class="node-value">' + node.value + '</div>' +
+            '<div class="left-node">' + leftNode + '</div>' +
+            '<div class="right-node">' + rightNode + '</div>' +
+            '</div>'
+    }
+
+    var rootEl = document.createElement("div");
+    rootEl.innerHTML = renderNode(this.tree);
+    return rootEl;
+};
+
+var binaryTree = new BinaryTree();
+binaryTree.create(10);
+
+
+console.log(binaryTree.tree);
+var binaryView = binaryTree.render();
+document.body.appendChild(binaryView);
