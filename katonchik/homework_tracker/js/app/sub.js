a05211f918aaeaeae2a0c7e5da847a374d9b32d9
katonchik <katonchik@gmail.com>
2015-01-28 11:43:34 +0200

    Skeleton per Getting Started guide

---
+++ katonchik/homework_tracker/js/app/sub.js
@@ -0,0 +1,16 @@
+/**
+ * Created by vkatolyk on 27.01.2015.
+ */
+define([], function () {
+    alert("inside sub.js");
+/*
+    var returnedModule = function () {
+        var _name = 'module1 name';
+        this.getName = function () {
+            return _name;
+        }
+    };
+
+    return returnedModule;
+*/
+});
