c933506a04d4db971514e364e23249066db21c2c
Vasyl <vatres@ukr.net>
2015-01-29 17:54:21 +0200

    update calendar and animation

--- vasyl/Calendar/source/script/calendar.js
+++ vasyl/Calendar/source/script/calendar.js
@@ -9,22 +9,44 @@ var Calendar = function () {
     this_.day;
     var date= new Date();
     var calendarContainer;
-
+    
+    /**
+     * set calendar date 
+     * @param Year
+     * @param Month
+     * @param Day
+     */
     this.setDate = function (Year, Month,Day) {
         this_.year = Year || this_.year || new Date().getFullYear();
         this_.month = Month || this_.month || new Date().getMonth() + 1;
         this_.day =Day || this_.day || new Date().getDate();
 
     };
-    function getDate() {
+    /**
+     * return first month day
+     * @returns {number}
+     */
+    function getDay() {
         return new Date(this_.year, this_.month - 1, 1).getDay();
     }
 
+    /**
+     * return number of days in defined month
+     * @param year
+     * @param month
+     * @returns {number}
+     */
     this.daysInMonth = function (year, month) {
         return new Date(year || this_.year, month || this_.month, 0).getDate();
     };
-    this.changeMonth = function (bool) {
-        if (bool) {
+
+    /**
+     * if switcher is true month++ else month--
+     * @param switcher
+     * @returns {number}
+     */
+    this.changeMonth = function (switcher) {
+        if (switcher) {
             this_.month++;
         } else {
             this_.month--;
@@ -39,15 +61,20 @@ var Calendar = function () {
         }
         return this.daysInMonth(this_.year, this_.month);
     };
+
+    /**
+     * draw calendar header
+     * @returns {HTMLElement}
+     */
     this.drawHeader = function () {
         var headerContainer = document.createElement("div");
         headerContainer.classList.add("header");
 
         var next = [">", "next", nextMonth];
         var previous = ["<", "previous", previousMonth];
-        function drawButton(parameter) {
+        function drawMonthScroll(parameter) {
             var button = document.createElement("button");
-            button.innerText = parameter[0];
+            button.innerHTML = parameter[0];
             button.classList.add("button", "button--" + parameter[1])
             headerContainer.appendChild(button)
             button.addEventListener("click", parameter[2])
@@ -55,14 +82,14 @@ var Calendar = function () {
 
         var inn = ["inn"];
         var out = ["output"];
-
-        function drawInput(parametr) {
+        // draw calendar title
+        function drawTitle(parameter) {
             var input = document.createElement("input");
             input.classList.add("input")
             headerContainer.appendChild(input)
-            if (parametr[0] == 'output') {
+            if (parameter[0] == 'output') {
                 input.setAttribute('disabled', false);
-                input.classList.add(parametr[0])
+                input.classList.add(parameter[0])
             } else {
                 input.addEventListener("keyup", function (event) {
                     set(event, input)
@@ -79,6 +106,12 @@ var Calendar = function () {
             this_.changeMonth(false)
             this_.update();
         }
+
+        /**
+         * set the calendar date from input element
+         * @param el
+         * @param element
+         */
         function set(el, element) {
             if (el.keyCode === 13) {
                 var a = element.value.split(".");
@@ -92,16 +125,18 @@ var Calendar = function () {
         }
 
         function init() {
-            drawButton(previous);drawButton(next)
-            drawInput(out);
-            drawInput(inn);
+            drawMonthScroll(previous);drawMonthScroll(next)
+            drawTitle(out);
+            drawTitle(inn);
 
         }
 
         init();
         return headerContainer;
     }
-
+    /**
+     * draw calendar body
+     */
     this.drawCalendar = function () {
         calendarContainer = document.createElement("div");
         calendarContainer.classList.add("calendar_container");
@@ -110,7 +145,7 @@ var Calendar = function () {
 
         function drawWeekDays() {
             var weakDays = document.createElement("div");
-            weakDays.classList.add("days-container");
+            weakDays.classList.add("days_container");
             daysNames.forEach(function (el) {
                 var dayOfWeak = document.createElement("div");
                 dayOfWeak.classList.add("weak");
@@ -122,13 +157,18 @@ var Calendar = function () {
 
         function drawDaysTable() {
             var daysContainer = document.createElement("div");
-            daysContainer.classList.add("days-container");
-            //event show selected day
-            daysContainer.addEventListener("click",function(event){
-                if(!event.toElement.classList.contains("day--empty")){
-                this_.setDate(this_.year,this_.month,event.toElement.innerHTML);
+            daysContainer.classList.add("days_container");
+            //add event that show selected day
+            daysContainer.addEventListener('click',function(event){
+                if(!event.target.classList.contains("day--empty")){
+                this_.setDate(this_.year,this_.month,event.target.innerHTML);
                 this_.update()}
             })
+            //add marked day
+            daysContainer.addEventListener('dblclick',function(event){
+                if(!event.target.classList.contains("day--empty")){
+                    event.target.classList.toggle("day--event")}
+            },true)
             for (var i = 1; i <= 42; i++) {
                 var days = document.createElement("div");
                 days.classList.add("day")
@@ -141,16 +181,20 @@ var Calendar = function () {
         drawWeekDays();
         drawDaysTable();
     };
-
+    /**
+     * clear calendar days
+     */
     function clear() {
-        var days = calendarContainer.getElementsByClassName("day")
-       // days.innerHTML==0;
+        var days = calendarContainer.getElementsByClassName("day") 
         var i;
         for (i = 1; i <= 42; i++) {
             days[i - 1].classList.remove("day--empty","day--current","day--selected");
         }
     }
 
+    /**
+     * update calendar
+     */
     this.update = function () {
         clear();
         var output = calendarContainer.getElementsByClassName("output")[0];
@@ -158,7 +202,7 @@ var Calendar = function () {
         var input= calendarContainer.getElementsByClassName("input")[1];
         input.value=this_.day+"."+this_.month+"."+this_.year;
         var days = calendarContainer.getElementsByClassName("day");
-        var firstDay = getDate();
+        var firstDay = getDay();
         var count = 1;
         var daysInMonth = this_.daysInMonth();
         var i;
